4949b4501902770c9f2ca9ab90880f8fb5df463d
Sébastien Velay <sebastien.velay@gmail.com>
2020-11-16 15:29:55 +0100

    improve navigation between component

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,25 @@
 <script>
   import Navbar from './components/Navbar.svelte';
   import Home from './components/Home.svelte';
+  import CreateGame from './components/CreateGame.svelte';
 
-  const createGame = (e) => {
-    alert('create a game');
+  let isCreateGame = false;
+
+  const createGame = () => {
+    isCreateGame = true;
+  };
+
+  const goHome = () => {
+    isCreateGame = false;
   };
 </script>
 
 <Navbar />
 
 <div class="wrapper">
-  <Home on:createGame={createGame} />
+  {#if isCreateGame == true}
+    <CreateGame on:goHome={goHome} />
+  {:else}
+    <Home on:createGame={createGame} />
+  {/if}
 </div>
