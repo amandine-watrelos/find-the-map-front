c562a9444d65ae7cbff2e3bd55e265e7a7c7b04c
Amandine Watrelos <amandine.watrelos@gmail.com>
2020-12-04 02:17:04 +0100

    feat(style) : homepage css for desktop version

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -16,30 +16,38 @@
 </script>
 
 <div class="container-fluid">
+  <div class="row">
+    <div id="img-container" class="col-md-6">
+      <img src="/images/map-detouree.png" alt="map-image" id="map-image"/>
+    </div>
 
-  <img src="/images/map-detouree.png" alt="map-image" id="map-image" class="col-md-2"/>
+    <div class="col-md-6 text-center">
+      <p id="game-rules-intro">Prove who has traveled the most in Dofus with Find the Map !<br></p>
+      <p class="text-justify" id="game-rules">
+        One game includes a list of dofus maps screenshots : for every map, you must indicate the position you think is correct.
+        If you are wrong, the difference between your proposal and the right answer will be taken into account.
+        At the end of the game, the winner is the one with the smallest positions difference for all the maps.</p>
+      <button on:click={goToGameCreation} type="button" class="btn">Create a new game</button>
+      <p>_________</p>
+      <form id="join-game-area">
+        <p>Join an existing game :</p>
 
-  <div class="col-md-2 text-center">
-    <button on:click={goToGameCreation} type="button" class="btn">Create a new game</button>
-    <p>_________</p>
-    <form id="join-game-area">
-      <p>Join an existing game :</p>
-
-      <div class="form-group">
-        <label for="playerName">Player name</label>
-        <div class="col-8">
-          <input type="text" bind:value={playerName} name="playerName" id="playerName" class="form-control">
+        <div class="form-group">
+          <label for="playerName">Player name</label>
+          <div class="col-8 col-md-4 d-md-inline-block">
+            <input type="text" bind:value={playerName} name="playerName" id="playerName" class="form-control">
+          </div>
         </div>
-      </div>
 
-      <div class="form-group">
-        <label for="gameId">Game ID</label>
-        <div class="col-9">
-          <input type="text" bind:value={gameId} name="gameId" id="gameId" class="form-control">
+        <div class="form-group">
+          <label for="gameId">Game ID</label>
+          <div class="col-9 col-md-4 d-md-inline-block">
+            <input type="text" bind:value={gameId} name="gameId" id="gameId" class="form-control">
+          </div>
         </div>
-      </div>
 
-      <button on:click={goToLobby} type="button" class="btn top-bot-margin">Play</button>
-    </form>
+        <button on:click={goToLobby} type="button" class="btn top-bot-margin">Play</button>
+      </form>
+    </div>
   </div>
 </div>
